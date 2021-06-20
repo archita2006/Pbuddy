cf393dd10ae66f0a37f1fb8d78707639ab9ebf3e
archita2006 <65909984+archita2006@users.noreply.github.com>
2021-06-20 11:02:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { createAppContainer,createSwitchNavigator } from "react-navigation";
+import{ Stylesheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+import React from 'react';
+export default class App extends React.Component{
+  render(){
+   return (
+<View>
+  
+</View>
+  
+   );
+}
+} 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+``  
